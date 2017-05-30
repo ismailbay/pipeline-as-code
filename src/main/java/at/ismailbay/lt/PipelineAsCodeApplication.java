cdc3d71fd1543c85bcc8184f5c32c5f96ff74129
Ismail Bay <ismail.bay@openforce.com>
2017-05-30 12:41:18 +0200

    initial version

---
+++ src/main/java/at/ismailbay/lt/PipelineAsCodeApplication.java
@@ -0,0 +1,12 @@
+package at.ismailbay.lt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAsCodeApplication.class, args);
+	}
+}
